e9b741116c6b8c65defea8a3019b41037ddf8f01
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2015-02-19 15:47:09 +0800

    add setup

--- pyyts/__init__.py
+++ pyyts/__init__.py
@@ -1,1 +1,9 @@
 from .pyyts_client import *
+
+__version__ = "1.0"
+__app_name__ = "pyyts"
+__description__ = "YTS API client"
+__author__ = "Ferdinand Silva"
+__author_email__ = "ferdinandsilva@ferdinandsilva.com"
+__app_url__ = "http://ferdinandsilva.com"
+__download_url__ = "https://github.com/six519/pyyts"
